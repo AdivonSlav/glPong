fcb57a78057d11e1dd4375caa58acf0a34cb6118
Adi <efendic100@gmail.com>
2022-03-02 10:41:22 +0100

    Finished collisions

--- Pong/src/game/GameLoop.cpp
+++ Pong/src/game/GameLoop.cpp
@@ -5,13 +5,10 @@
 #include "../graphics/Renderer.h"
 
 using namespace PongMaths;
+using namespace PongCore::PongConst;
 
 namespace PongCore
 {
-	constexpr float MOVEMENT_SPEED = 200.0f;
-	constexpr float MOVEMENT_OFFSET = 10.0f;
-	const Vec2 INIT_VELOCITY = Vec2(150.0f, 150.0f);
-
 	void Pong::Init()
 	{
 		window = new PongGraphics::Window("Pong", 960, 540.0f);
@@ -19,7 +16,7 @@ namespace PongCore
 		window->SetShader(shader);
 		paddle1 = new PongGraphics::RenderableObject("Paddle1",Vec3(15, 220, 0), Vec2(20, 100), Vec4(1.0f, 1.0f, 1.0f, 1.0f), *shader);
 		paddle2 = new PongGraphics::RenderableObject("Paddle2", Vec3(925, 220, 0), Vec2(20, 100), Vec4(1.0f, 1.0f, 1.0f, 1.0f), *shader);
-		ball = new Ball(Vec3(480.0f, 270.0f, 1.0f), Vec4(1.0f, 1.0f, 1.0f, 1.0f), 50, 10, INIT_VELOCITY, *shader);
+		ball = new Ball(Vec3(480.0f, 270.0f, 1.0f), Vec4(1.0f, 1.0f, 1.0f, 1.0f), 50, 8, BALL_INIT_VELOCITY, *shader);
 	}
 
 	void Pong::ProcessInput(double timeDelta)
@@ -27,17 +24,17 @@ namespace PongCore
 		if (window->IsKeyPressed(GLFW_KEY_ESCAPE))
 			glfwSetWindowShouldClose(window->GetWindow(), true);
 
-		if (window->IsKeyPressed(GLFW_KEY_UP) && paddle2->GetPosition().y + paddle2->GetSize().y < window->GetHeight() - MOVEMENT_OFFSET)
-			paddle2->SetVelocity(Vec2(0.0f, MOVEMENT_SPEED));
-		else if (window->IsKeyPressed(GLFW_KEY_DOWN) && paddle2->GetPosition().y > 0.0f + MOVEMENT_OFFSET)
-			paddle2->SetVelocity(Vec2(0.0f, -MOVEMENT_SPEED));
+		if (window->IsKeyPressed(GLFW_KEY_UP) && paddle2->GetPosition().y + paddle2->GetSize().y < window->GetHeight() - PADDDLE_OFFSET)
+			paddle2->SetVelocity(Vec2(0.0f, PADDLE_SPEED));
+		else if (window->IsKeyPressed(GLFW_KEY_DOWN) && paddle2->GetPosition().y > 0.0f + PADDDLE_OFFSET)
+			paddle2->SetVelocity(Vec2(0.0f, -PADDLE_SPEED));
 		else
 			paddle2->SetVelocity(Vec2(0.0f, 0.0f));
 
-		if (window->IsKeyPressed(GLFW_KEY_W) && paddle1->GetPosition().y + paddle1->GetSize().y < window->GetHeight() - MOVEMENT_OFFSET)
-			paddle1->SetVelocity(Vec2(0.0f, MOVEMENT_SPEED));
-		else if (window->IsKeyPressed(GLFW_KEY_S) && paddle1->GetPosition().y > 0.0f + MOVEMENT_OFFSET)
-			paddle1->SetVelocity(Vec2(0.0f, -MOVEMENT_SPEED));
+		if (window->IsKeyPressed(GLFW_KEY_W) && paddle1->GetPosition().y + paddle1->GetSize().y < window->GetHeight() - PADDDLE_OFFSET)
+			paddle1->SetVelocity(Vec2(0.0f, PADDLE_SPEED));
+		else if (window->IsKeyPressed(GLFW_KEY_S) && paddle1->GetPosition().y > 0.0f + PADDDLE_OFFSET)
+			paddle1->SetVelocity(Vec2(0.0f, -PADDLE_SPEED));
 		else
 			paddle1->SetVelocity(Vec2(0.0f, 0.0f));
 	}
@@ -56,39 +53,36 @@ namespace PongCore
 			newVelocity.Multiply(Vec2(1.0f, -1.0f));
 			ball->SetVelocity(newVelocity);
 		}
-		//
 
 		// Collision with the left and right screen border
-		bool toReset = false;
+		char toReset = 0;
 
 		if (ball->GetPosition().x - ball->GetRadius() <= 0)
 		{
 			std::cout << "Right player scores!" << std::endl;
-			toReset = true;
+			toReset = 'R';
 		}
 		else if (ball->GetPosition().x + ball->GetRadius() >= window->GetWidth())
 		{
 			std::cout << "Left player scores!" << std::endl;
-			toReset = true;
+			toReset = 'L';
 		}
 
 		if (toReset)
-		{
-			Reset();
-			toReset = false;
-		}
-		//
+			Reset(toReset);
 
 		// Paddle collision
 		if (ball->GetPosition().x > window->GetWidth() / 2.0f)
 			PaddleCollision(*paddle2, timeDelta);
 		else
 			PaddleCollision(*paddle1, timeDelta);
-		//
 	}
 
 	void Pong::PaddleCollision(const PongGraphics::RenderableObject& paddle, double timeDelta)
 	{
+		if (frameCollisionCount != -1)
+			frameCollisionCount++;
+
 		Vec3 distance = ball->GetPosition();
 		bool paddleCollide = false;
 
@@ -97,54 +91,53 @@ namespace PongCore
 		distance.x = std::abs(distance.x);
 		distance.y = std::abs(distance.y);
 
+		// Collision only happens if a certain number of frames contained a collision, as defined by the treshold constant
 		// If the distance between the ball and the paddle is shorter or equal to the size of the paddle and the ball radius, then we know a collision has occured
-		if (distance.x <= paddle.GetSize().x + ball->GetRadius() && distance.y <= paddle.GetSize().y + ball->GetRadius())
+		if (frameCollisionCount >= COLLISION_FRAME_TRESHOLD || frameCollisionCount == -1)
 		{
-			if (distance.x <= paddle.GetSize().x)
+			if (distance.x <= paddle.GetSize().x / 2.0f + ball->GetRadius() && distance.y <= paddle.GetSize().y / 2.0f + ball->GetRadius())
 			{
-				paddleCollide = true;
-				Vec2 newVelocity = ball->GetVelocity();
-				newVelocity.Multiply(Vec2(-1.0f, 1.0f));
-				ball->SetVelocity(newVelocity);
-			}
-			else if (distance.y <= paddle.GetSize().y)
-			{
-				paddleCollide = true;
-				Vec2 newVelocity = ball->GetVelocity();
-				newVelocity.Multiply(Vec2(1.0f, -1.0f));
-				ball->SetVelocity(newVelocity);
-			}
-
-			Vec3 squaredDistance = distance - Vec3(paddle.GetSize().x / 2.0f, paddle.GetSize().y / 2.0f, 0.0f);
-			squaredDistance.Multiply(squaredDistance);
-
-			if (squaredDistance.x + squaredDistance.y <= std::pow(ball->GetRadius(), 2))
-			{
-				paddleCollide = true;
-				Vec2 newVelocity = ball->GetVelocity();
-				newVelocity.Multiply(Vec2(-1.0f, 1.0f));
-				ball->SetVelocity(newVelocity);
+				if (distance.x <= paddle.GetSize().x && distance.x <= paddle.GetSize().x + ball->GetRadius())
+				{
+					paddleCollide = true;
+					Vec2 newVelocity = ball->GetVelocity();
+					newVelocity.Multiply(Vec2(-1.0f, 1.0f));
+					ball->SetVelocity(newVelocity);
+				}
+				else if (distance.y <= paddle.GetSize().y && distance.y <= paddle.GetSize().y + ball->GetRadius())
+				{
+					paddleCollide = true;
+					Vec2 newVelocity = ball->GetVelocity();
+					newVelocity.Multiply(Vec2(1.0f, -1.0f));
+					ball->SetVelocity(newVelocity);
+				}
 			}
 		}
 
 		if (paddleCollide)
 		{
+			// Slight increase in ball velocity after collision
 			Vec2 newVelocity = ball->GetVelocity();
-			newVelocity.Add(Vec2(0.0f, 0.4f * paddle.GetVelocity().y));
+			newVelocity.Add(Vec2(0.0f, 0.6f * paddle.GetVelocity().y));
+			newVelocity.Multiply(Vec2(1.03f, 1.0f));
 			ball->SetVelocity(newVelocity);
 
-			paddleCollide = false;
+			frameCollisionCount = 0;
 		}
 	}
 
-	void Pong::Reset()
+	void Pong::Reset(char& toReset)
 	{
+		// The starting ball trajectory is reversed towards the player that last scored
+
 		ball->SetPosition(Vec3(window->GetWidth() / 2.0f, window->GetHeight() / 2.0f, 1.0f));
-		ball->SetVelocity(INIT_VELOCITY);
+		ball->SetVelocity(toReset=='L'?Vec2()- BALL_INIT_VELOCITY : BALL_INIT_VELOCITY);
+		toReset = 0;
+		frameCollisionCount = 0;
 	}
 
 	Pong::Pong()
-		: window(nullptr), shader(nullptr), paddle1(nullptr), paddle2(nullptr), ball(nullptr)
+		: window(nullptr), shader(nullptr), paddle1(nullptr), paddle2(nullptr), ball(nullptr), frameCollisionCount(-1)
 	{
 		Init();
 	}
--- Pong/src/game/GameLoop.h
+++ Pong/src/game/GameLoop.h
@@ -5,6 +5,16 @@
 #include "../graphics/Window.h"
 #include "../graphics/RenderableObject.h"
 
+namespace PongCore { namespace PongConst {
+
+	constexpr float PADDLE_SPEED = 230.0f;
+	constexpr float PADDDLE_OFFSET = 10.0f;
+	const PongMaths::Vec2 BALL_INIT_VELOCITY = PongMaths::Vec2(250.0f, 50.0f);
+
+	constexpr int COLLISION_FRAME_TRESHOLD = 10;
+}}
+
+
 namespace PongCore
 {
 	class Pong
@@ -16,12 +26,15 @@ namespace PongCore
 		PongGraphics::RenderableObject* paddle2;
 		Ball* ball;
 
+		int frameCollisionCount;
+
 		void Init();
 
 		void ProcessInput(double timeDelta);
 		void UpdatePosition(double timeDelta);
 		void PaddleCollision(const PongGraphics::RenderableObject& paddle, double timeDelta);
-		void Reset();
+
+		void Reset(char& toReset);
 	public:
 		Pong();
 		~Pong();
